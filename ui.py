3aefb38a731afc9749faeafd5b14d74062e8bace
SwissyT <SwissyT@imamac4.local>
2021-02-03 18:46:16 +0100

    Done UI on ANN.

--- ui.py
+++ ui.py
@@ -11,6 +11,8 @@ if sys.platform == 'darwin':
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import argparse
+import uuid
+import pickle
 
 class ANN(Model):
     def __init__(self, X_dim, Y_dim, activation):
@@ -37,7 +39,7 @@ class ANN_wrapper:
 
     def se(self, y_pred, y_true):
         loss = tf.keras.losses.mean_squared_error(y_true, y_pred)
-        return tf.reduce_sum(loss)
+        return tf.reduce_mean(loss)
 
     def run_optimization(self, x, y):
         with tf.GradientTape() as g:
@@ -82,6 +84,8 @@ def main():
     print(sys.argv)
     print(args)
 
+    assert (args.n_train % 2 == 0), 'provide an even number of training instances.'
+
     loc_noise = args.loc_noise
     scale_noise = args.scale_noise
     n_train = args.n_train
@@ -91,23 +95,23 @@ def main():
     max_epochs = args.max_epochs
     activation = args.activation
 
-    X_train, Y_train = generate_data(n_train, loc_noise, scale_noise)
+    half = n_train // 2
+
+    X_train_D0, Y_train_D0 = generate_data(half, loc_noise, scale_noise)
     X_validation, Y_validation = generate_data(n_validation, loc_noise, scale_noise)
     #X_test, Y_test = generate_data(n_test, loc_noise, scale_noise)
 
-    train_data = tf.data.Dataset.from_tensor_slices((X_train, Y_train))
+    train_data = tf.data.Dataset.from_tensor_slices((X_train_D0, Y_train_D0))
     train_data = train_data.repeat().batch(batch_size)
 
-    max_steps = int(n_train / batch_size) * max_epochs
+    max_steps = int(half / batch_size) * max_epochs
 
-    ann_wrapper = ANN_wrapper(ann=ANN(X_dim=X_train.shape[-1], Y_dim=Y_train.shape[-1], activation=activation),
+    ann_wrapper = ANN_wrapper(ann=ANN(X_dim=X_train_D0.shape[-1], Y_dim=Y_train_D0.shape[-1], activation=activation),
                               optimizer=tf.keras.optimizers.Adam())
 
-    X_test = np.repeat(np.expand_dims(np.linspace(-5., 5., 10000), axis=-1), X_train.shape[-1], axis=-1)
+    X_test = np.repeat(np.expand_dims(np.linspace(-5., 5., 10000), axis=-1), X_train_D0.shape[-1], axis=-1)
 
     pred_before = ann_wrapper.ann(X_test)
-
-
 
     prev_validation_error = np.inf
     epoch = 0
@@ -118,78 +122,49 @@ def main():
             pbar.update(1)
             if step % 1000 == 0:
                 print(loss)
-            if step % int(n_train / batch_size) == 0:
+            if step % int(half / batch_size) == 0:
                 epoch += 1
                 validation_error = ann_wrapper.get_error(X_validation, Y_validation).numpy()
                 print('Epoch: %i, step: %i, validation error: %f' % (epoch, step, validation_error))
                 early_stopping = early_stopping + 1 if prev_validation_error - validation_error <= 1e-5 else 0
-                if early_stopping == 5:
-                    print('Validation error did not fall below 1e-5 for 5 consecutive epochs; applying early stopping.')
+                if early_stopping == 20:
+                    print('Validation error did not fall below 1e-5 for 20 consecutive epochs; applying early stopping.')
                     break
                 prev_validation_error = validation_error
-    #print('Done training.')
-    #print('Test error (mean): %f.' % ann_wrapper.get_error(X_test, Y_test).numpy() / float(len(X_test)))
-    exit()
 
+    # UI
+    alpha = .05
+    X_train_D1, Y_train_D1 = generate_data(half, loc_noise, scale_noise)
 
-    pred_after0 = ann_wrapper.ann(np.stack([X_test[:, 0], np.zeros(X_test.shape[0])], axis=-1))
-    plt.figure()
-    plt.plot(X_test[:, 0], pred_after0)
-    plt.title('first dim')
-    plt.grid()
+    pred = ann_wrapper.ann(X_train_D1).numpy()
+    denom = np.exp(-.5 * np.linalg.norm(Y_train_D1 - pred)**2.)
 
-    pred_after1 = ann_wrapper.ann(np.stack([np.zeros(X_test.shape[0]), X_test[:, 1]], axis=-1))
-    plt.figure()
-    plt.plot(X_test[:, 1], pred_after1)
-    plt.title('second dim')
-    plt.grid()
+    # ----
+    original_mask = ann_wrapper.ann.mask.numpy()
 
+    Values = [np.linspace(-10., 10., 20001), np.linspace(-10., 10., 20001) + 1.]
+    Values = np.stack(Values, axis=0)
+    rejections = np.zeros_like(Values)
 
+    for i in tqdm(range(X_train_D0.shape[-1])):
+        values = Values[i]
+        mask = original_mask.copy()
+        for j in tqdm(range(len(values))):
+            mask[i] = values[j]
+            ann_wrapper.ann.mask.assign(mask)
 
+            pred = ann_wrapper.ann(X_train_D1).numpy()
+            numer = np.exp(-.5 * np.linalg.norm(Y_train_D1 - pred)**2.)
 
-    plt.show()
-    print(ann_wrapper.ann.mask)
-    print(ann_wrapper.ann.hidden)
-    print(ann_wrapper.ann.out)
+            T = numer / denom
 
+            rejections[i, j] = 1. if T > 1. / alpha else 0.
 
+    filename = str(uuid.uuid4()) + '.pickle'
+    print(filename)
 
-    exit()
-
-
-    pred_after = ann(X_test)
-
-
-    plt.figure()
-    plt.plot(X_test[:, 0], np.sin(5. * X_test[:, 0]) / (5. * X_test[:, 0]), label='true')
-    plt.plot(X_test[:, 0], pred_before, label='before')
-    plt.plot(X_test[:, 0], pred_after, label='after')
-
-    plt.title('first dim')
-    plt.legend()
-    plt.grid()
-
-    plt.figure()
-    plt.scatter(X_test[:, 1], np.sin(5. * X_test[:, 0]) / (5. * X_test[:, 0]), label='true')
-    plt.scatter(X_test[:, 1], pred_before, label='before')
-    plt.scatter(X_test[:, 1], pred_after, label='after')
-
-    plt.title('second dim')
-    plt.legend()
-    plt.grid()
-
-
-
-
-    plt.show()
-
-
-
-
-
-
-
-
+    d = {'Values': Values, 'rejections': rejections}
+    pickle.dump(d, open(filename, 'wb'))
 
 if __name__ == '__main__':
     main()
